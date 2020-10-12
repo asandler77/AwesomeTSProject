834cf9f665add1f37099569c3d7661db1d0672f9
Sandler, Alexey(as371x) <as371x@intl.att.com>
2020-10-12 21:05:21 +0300

    Initial commit.

---
+++ App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View,} from 'react-native';
+import { Component } from 'react';
+
+
+export default function App() {
+    return(
+        <View style={styles.body}>
+          <Text>Kukulkhlhlih</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#CCFFE5',
+    height: '100%',
+    margin: 0,
+  }
+});
+
