b19bc401ca8540b5d24f6984d86b55d28e938e1f
Sandler, Alexey(as371x) <as371x@intl.att.com>
2020-11-01 22:04:48 +0200

    Add header part test file.(one test is failed)

---
+++ __tests__/HeaderPart.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {ProgressBar, UsagePerUser} from '../components/ProgressBar';
+import {shallow} from 'enzyme';
+import {createUsagePerUserData} from "./utils.test";
+import {ProgressBarPart} from "../components/ProgressBarPart";
+import {HeaderPart} from "../components/HeaderPart";
+
+
+
+
+describe('ProgressBar component ', () => {
+
+    const usagePerUserData = createUsagePerUserData();
+
+    beforeEach(() => {
+    });
+
+    const wrapper = shallow(
+        <HeaderPart
+            usagePerUserData={usagePerUserData}
+            currentUserIndex={0}
+            usageLimitWarningLevel={60}
+        />)
+
+    test('renders warning icon', () => {
+        expect(wrapper.find(`[testID='warning_image_testID']`)).toHaveLength(1);
+    })
+
+    test('does not render warning icon', () => {
+        wrapper.setProps({usageLimitWarningLevel: 50});
+        expect(wrapper.find(`[testID='warning_image_testID']`)).toHaveLength(0);
+
+    })
+})
---
+++ __tests__/utils.test.ts
@@ -0,0 +1,10 @@
+import {UsagePerUser} from "../components/ProgressBar";
+
+export const createUsagePerUserData = (): UsagePerUser[] => {
+    let usagePerUserData: UsagePerUser[] = [];
+    usagePerUserData.push({userName: "Alexey", usage: 5});
+    usagePerUserData.push({userName: "John", usage: 10});
+    usagePerUserData.push({userName: "Bill", usage: 20});
+    usagePerUserData.push({userName: "Anton", usage: 15});
+    return usagePerUserData;
+}
